127db0c2ff2674f9450b53a96ecf02e2419517a5
Geoffrey Tan <geoffreyt@live.nl>
2018-12-20 23:26:57 +0100

    Mysql connection

--- index.js
+++ index.js
@@ -2,14 +2,33 @@ const express = require('express')
 const app = express();
 let port = process.env.PORT;
 
-if (port == null || port == "") {
+if (port == null || port == "") { // Changes port to 8000 when opened locally
   port = 8000;
 }
 
+/* -- MYSQL -- */
+var mysql = require('mysql');
+var myConnection = require('express-myconnection');
+var dbOptions = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_USER,
+  port: 3306,
+  database: 'db_test'
+};
+
+app.use(myConnection(mysql, dbOptions, 'single'));
+
+/* -- Routes -- */
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! ' + process.env.DB_USER)
 });
 
+app.get('/login', (req, res) => {
+  res.send('Login page')
+});
+
+/* -- Port -- */
 app.listen(port, () => {
-  console.log('Example app listening on port 8000!')
+  console.log('App listening on port ' + port)
 });
